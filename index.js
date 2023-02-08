aa3a5b95fc2846b63974bda1c403ff828b0b49e4
Olga <olga.artems@gmail.com>
2023-02-08 16:13:51 +0300

    remove music file, enhance code style

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
 /**
  * Module dependencies.
  */
-var express = require('express');
-var httpProxy = require('http-proxy');
-var bodyParser = require('body-parser');
+const express = require('express');
+const httpProxy = require('http-proxy');
+const bodyParser = require('body-parser');
 
-var port = +process.argv[2] || 8080;
+const port = +process.argv[2] || 8080;
 
-var proxyOptions = {
+const proxyOptions = {
   changeOrigin: true,
-
   ignorePath: true
 };
 
-httpProxy.prototype.onError = function (err) {
+httpProxy.prototype.onError = function(err) {
   console.log(err);
 };
 
-var apiProxy = httpProxy.createProxyServer(proxyOptions);
+const apiProxy = httpProxy.createProxyServer(proxyOptions);
 
-// Node express server setup.
-var server = express();
-
+// Node express server setup
+const server = express();
 server.set('port', port);
 server.use(express.static(__dirname + '/'));
-
 server.use(bodyParser.json());
-
 server.use(bodyParser.urlencoded({
   extended: true
 }));
 
 server.all("/download", function(req, res) {
-  console.log('Intercepting request');
-
-  console.log(req.query.url);
-
+  console.log('Intercepting request', req.query.url);
   apiProxy.web(req, res, {target: req.query.url});
 });
 
-// Start Server.
+// Start Server
 server.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
--- player.js
+++ player.js
@@ -1,154 +1,104 @@
-var initPlayer = function(player) {
+const initPlayer = function(player) {
 	player.ctxt = new window.AudioContext();
-
 	player.source = player.ctxt.createBufferSource();
-
 	player.gainNode = player.ctxt.createGain();
-
 	player.analyser = player.ctxt.createAnalyser();
 };
 
-var player = {
+const player = {
 	source: null,
-
 	currentTime: 0,
-
 	gainNode: null,
-
 	ctxt: null,
 
 	start: function() {
 		this.source.start(this.currentTime);
 	},
-
 	pause: function() {
 		this.ctxt.suspend();
 	},
-
 	resume: function() {
 		this.ctxt.resume();
 	},
-
 	stop: function() {
 		this.currentTime = this.ctxt.currentTime;
-
 		this.source.stop();
 	},
-
 	setVolume: function(volumeLevel) {
 		if( volumeLevel < 0 ) {
 			console.warn('Can not set negative volume level');
-
 			return;
 		}
-
 		this.gainNode.gain.value = volumeLevel / 100;
 	},
-
 	analyse: function() {
 		player.analyser.smoothingTimeConstant = 0.3;
-
 		player.analyser.fftSize = 512;
-
 		draw();
 	}
 };
 
 initPlayer(player);
 
-var loadFile = function(url) {
-	var xhr = new XMLHttpRequest();
-
+const loadFile = function(url) {
+	const xhr = new XMLHttpRequest();
 	xhr.open('GET', url, true);
-
 	xhr.responseType = 'arraybuffer';
-
 	xhr.onload = function() {
 		player.ctxt.decodeAudioData(this.response)
 				.then(function(decodedData) {
 					getDuration(decodedData);
-
 					play(decodedData);
-
 					player.analyse();
-
 				}, function(e) {
 					console.error('Error decoding file: ', e);
 				});
 	};
-
 	xhr.send();
 };
 
 // Load file
+loadFile(''); // your file here
 
-loadFile('apparat_fractales.mp3');
-
-var play = function(decodedData) {
+const play = function(decodedData) {
 	player.source.buffer = decodedData;
-
 	// Set connections
-
 	player.source.connect(player.gainNode);
-
 	player.source.connect(player.analyser);
-
 	player.gainNode.connect(player.ctxt.destination);
-
 	// No-repeat
-
 	player.source.loop = false;
-
 	// Start playback
-
 	player.start(0);
-
 	// Handlers settings
-
 	player.setVolume(document.getElementById('player-volume').value);
-
 	document.getElementById('play').style.display = "none";
-
 	document.getElementById('pause').style.display = "inline-block";
 };
 
-var stop = function() {
-	player.stop();
-};
-
-var getDuration = function(decodedData) {
-	var minutes = Math.floor(decodedData.duration / 60);
-
-	var seconds = ((decodedData.duration % 60)).toFixed(0);
-
+const getDuration = function(decodedData) {
+	const minutes = Math.floor(decodedData.duration / 60);
+	let seconds = ((decodedData.duration % 60)).toFixed(0);
 	seconds = seconds > 9 ? seconds: "0" + seconds;
-
 	document.getElementById("duration").innerHTML = minutes + ':' + seconds;
 };
 
 // Handlers
-
 document.getElementById('play').addEventListener('click', function() {
 	player.resume();
-
 	document.getElementById('pause').style.display = "inline-block";
-
 	this.style.display = "none";
 });
 
 document.getElementById('pause').addEventListener('click', function() {
 	player.pause();
-
 	document.getElementById('play').style.display = "inline-block";
-
 	this.style.display = "none";
 });
 
 document.getElementById('stop').addEventListener('click', function() {
 	player.stop();
-
 	document.getElementById('pause').style.display = "none";
-
 	document.getElementById('play').style.display = "inline-block";
 });
 
@@ -156,38 +106,24 @@ document.getElementById('player-volume').addEventListener('input', function() {
 	player.setVolume(this.value);
 });
 
-var canvas = document.createElement('canvas');
-
+const canvas = document.createElement('canvas');
 canvas.width = 600;
-
 canvas.height = 200;
-
 document.getElementById('widget-wrapper').appendChild(canvas);
-
-var canvasCtx = canvas.getContext('2d');
-
+const canvasCtx = canvas.getContext('2d');
 canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
 function draw() {
-	var bufferLength = player.analyser.frequencyBinCount;
-
-	var dataArray = new Float32Array(bufferLength);
-
+	const bufferLength = player.analyser.frequencyBinCount;
+	const dataArray = new Float32Array(bufferLength);
 	player.analyser.getFloatFrequencyData(dataArray);
-
 	canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-
 	canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-	var barWidth = 10, barHeight, x = 0;
-
-	for(var i = 0; i < bufferLength; i++) {
+	let barWidth = 10, barHeight, x = 0;
+	for(let i = 0; i < bufferLength; i++) {
 		barHeight = (dataArray[i] + 100) * 3;
-
 		canvasCtx.fillStyle = 'rgb(30,' + Math.floor(barHeight) + ',140)';
-
 		canvasCtx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
-
 		x += barWidth + 1;
 	}
 
